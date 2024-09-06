87f9d18d67b4b1cc8d88ac60a8f4244bb5b91c1e
LeonelGrosse <leonelgrosse@outlook.com>
2024-09-06 14:35:50 -0300

    Clase Categoria

---
+++ TPWinForms/Catalogo/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogo
+{
+    internal class Categoria
+    {
+        public string categoria { get; set; }
+    }
+}
